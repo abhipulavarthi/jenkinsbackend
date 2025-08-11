bb0d4fa0ff8751b5d95552272e36b3ce1028df48
lazypandaa <eswarkrishnak2005@gmail.com>
2025-08-11 10:40:13 +0530

    first commit

---
+++ src/main/java/com/klu/controller/ArthmaticCalc.java
@@ -0,0 +1,36 @@
+package com.klu.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin("*")
+public class ArthmaticCalc {
+	@GetMapping("add/{A}/{B}")
+	public String add(@PathVariable("A") int a,@PathVariable("B") int b) {
+		return "Addition= " +(a+b);
+	}
+	
+	 @GetMapping("sub/{A}/{B}")
+	    public String sub(@PathVariable("A") int a, @PathVariable("B") int b) {
+	        return "Subtraction = " + (a - b);
+	    }
+	 
+	 @GetMapping("mul/{A}/{B}")
+	    public String mul(@PathVariable("A") int a, @PathVariable("B") int b) {
+	        return "Multiplication = " + (a * b);
+	    }
+	 @GetMapping("div/{A}/{B}")
+	    public String div(@PathVariable("A") int a, @PathVariable("B") int b) {
+	        return "Divition = " + (a / b);
+	    }
+	 @GetMapping("mod/{A}/{B}")
+	    public String mod(@PathVariable("A") int a, @PathVariable("B") int b) {
+	        return "Mod = " + (a % b);
+	    }
+
+}

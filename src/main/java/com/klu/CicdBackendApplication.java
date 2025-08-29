2ca4c492606a50bd02d428cfa993cfcc8a37732a
drkpnabhipulavarthi <abhinav.pulavarthi06@gmail.com>
2025-08-30 00:28:07 +0530

    first commit

--- src/main/java/com/klu/CicdBackendApplication.java
+++ src/main/java/com/klu/CicdBackendApplication.java
@@ -2,9 +2,10 @@ package com.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdBackendApplication {
+public class CicdBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdBackendApplication.class, args);
